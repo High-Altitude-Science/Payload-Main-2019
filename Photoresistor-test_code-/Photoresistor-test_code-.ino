9e7662e7c4cf2c18042e950f585b618fc5b1f91c
TopazJ <shamez.meghji1@ucalgary.ca>
2019-11-30 14:40:15 -0700

    Modified, uploaded, and tested photoresistor code
    Photoresistor seems sensitive to change, no real pattern found.

--- Photoresistor-test_code-/Photoresistor-test_code-.ino
+++ Photoresistor-test_code-/Photoresistor-test_code-.ino
@@ -1,20 +1,14 @@
-int GPIO_PWM_A = 36;
 int GPIO_ANALOG_A = 37;
 
-void setup()   {                
-  pinMode(GPIO_PWM_A, OUTPUT);
+void setup()   {
   Serial.begin(38400);
 }
 
-int i = 0;
 int photoVoltageRaw = 0;
 
 void loop()                     
 {
-  analogWrite(GPIO_PWM_A,i++);
   photoVoltageRaw = analogRead(GPIO_ANALOG_A);
-  if (i == 255)
-    i = 0;
   Serial.println(photoVoltageRaw);
   delay(500);
   
