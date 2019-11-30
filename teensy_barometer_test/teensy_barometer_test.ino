a3d5d67f5ab2f5b086a02f356bd1ef9556f9d18b
TopazJ <shamez.meghji1@ucalgary.ca>
2019-11-30 14:16:20 -0700

    Modified code so teensy can read sensor.
    Code still needs further modifcation, as we will need to change the i2c library and Altimiter sensor library we are using to read from multiple i2c sensors.

--- teensy_barometer_test/teensy_barometer_test.ino
+++ teensy_barometer_test/teensy_barometer_test.ino
@@ -1,9 +1,20 @@
 
 #include <Wire.h>
+//#include <i2c_t3.h>
+#include <Adafruit_MPL3115A2.h>
 
-int address1 = 72;
+//TODO This works, but we need to use the i2c_t3 library commented out above for multiple I2C connections.
+//If we are using the i2c library, we will need to make our own version of the Adafruit_MPL3115A2 code so we can
+//receive a different type of Wire object.
+
+// Power by connecting Vin to 3-5V, GND to GND
+// Uses I2C - connect SCL to the SCL pin, SDA to SDA pin
+// See the Wire tutorial for pinouts for each Arduino
+// http://arduino.cc/en/reference/wire
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
 int pin_SDA = 18; 
-int pin_SCL = 19; 
+int pin_SCL = 19;
 
 void setup() 
 {
@@ -15,18 +26,23 @@ void setup()
   Serial.println("Begin!");
 }
 
-void loop() 
-{
-  int c;
 
-  Wire.beginTransmission(address1);  
-  Wire.write(0);  
-  Wire.endTransmission();  
-  Wire.requestFrom(address1, 2);
+void loop() {
+  if (! baro.begin(&Wire)) {
+    Serial.println("Couldnt find sensor");
+    return;
+  }
   
-  c = Wire.receive();
-  
-  Serial.print("Temperature: ");
-  Serial.println(c);
+  float pascals = baro.getPressure();
+  // Our weather page presents pressure in Inches (Hg)
+  // Use http://www.onlineconversion.com/pressure.htm for other units
+  Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+
+  float altm = baro.getAltitude();
+  Serial.print(altm); Serial.println(" meters");
+
+  float tempC = baro.getTemperature();
+  Serial.print(tempC); Serial.println("*C");
+
   delay(1000);
 }
